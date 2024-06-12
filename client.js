af2783953734085e39ba1424c8dc6fd3605ad87a
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-11 21:01:19 -0600

    Modify code to be able to send data to server

--- client.js
+++ client.js
@@ -5,6 +5,21 @@ const connect = function () {
     port: 50541, // PORT number here,
   });
 
+  conn.on("connect", () => {
+    console.log("Succesfully connected to game server");
+  });
+
+  // set the encoding on standard in
+  process.stdin.setEncoding("utf-8");
+
+  // listen for information coming from standard in (terminal)
+  process.stdin.on("data", (data) => {
+    console.log("Name: ", data);
+
+    // send the information to the server
+    conn.write(data);
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
