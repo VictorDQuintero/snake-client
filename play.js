df1d56be6dd356533bd13d158a0759d7a1d12e9e
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-12 15:58:58 -0600

    Make connect function pass its return value to setupInput function, Add comments

--- play.js
+++ play.js
@@ -4,5 +4,6 @@ const setupInput = require("./input");
 // establishes a connection with the game server
 
 console.log("Connecting ...");
-connect();
-setupInput();
+setupInput(connect()); // connect returns an object that can be used to interact with the server and it is passed to setupInput
+// setupInput() places a reference to that object in another variable connection which is in a global scope of that module
+// When data comes in from your keyboard, the stdin event handler can now interact with the server because the scope in the handler now includes both data from the keyboard AND the connection object!
