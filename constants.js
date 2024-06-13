dfadb6aa1e9ebe77549cfcaf01b37537c5ee9e13
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-13 17:17:51 -0600

    Delete commented out code

--- constants.js
+++ constants.js
@@ -1,17 +1,5 @@
 const IP = "localhost";
 const PORT = 50541;
-// const MOVE_KEYS = {
-//   MOVE_UP_KEY: ["w", "Move: up"],
-//   MOVE_DOWN_KEY: ["s", "Move: down"],
-//   MOVE_LEFT_KEY: ["a", "Move: left"],
-//   MOVE_RIGHT_KEY: ["d", "Move: right"],
-// };
-/* const MOVE_KEYS = {
-  MOVE_UP_KEY:  { letter: "w", command: "Move: up" } ,
-  MOVE_DOWN_KEY: { letter: "s", command: "Move: down" },
-  MOVE_LEFT_KEY: { letter: "a", command: "Move: left" },
-  MOVE_RIGHT_KEY: { letter: "d", command: "Move: right" },
-}; */
 
 const MOVE_KEYS = {
   w: "Move: up",
@@ -21,10 +9,10 @@ const MOVE_KEYS = {
 };
 
 const MESSAGES = {
-  MESSAGE1: { letter: "l", message: "Say: LOL" },
-  MESSAGE2: { letter: "g", message: "Say: Gotcha!" },
-  MESSAGE3: { letter: "y", message: "Say: Yum!" },
-  MESSAGE4: { letter: "n", message: "Say: Nooo!" },
+  l: "Say: LOL",
+  g: "Say: Gotcha!",
+  y: "Say: Yum!",
+  n: "Say: Nooo!",
 };
 
 module.exports = {
