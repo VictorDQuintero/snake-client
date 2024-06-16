2e47d8ae9aa6a845511cfdc05c0733dbf56df490
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-15 21:53:19 -0600

    Delete some comments

--- input.js
+++ input.js
@@ -1,9 +1,9 @@
 // setup interface to handle user input from stdin
-let connection; // outermost-scope:global scope
+let connection;
 // setupInput() places a reference to that object in another variable connection which is in a global scope of that module
 const { MOVE_KEYS, MESSAGES } = require("./constants");
 
-const setupInput = function(conn) {
+const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin; // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
@@ -11,13 +11,15 @@ const setupInput = function(conn) {
 
   stdin.resume(); // resume stdin so the program can listen for input
   stdin.on("data", handleUserInput); // the terminal listens to user input and a callback function acts on that data
-  // or more explicitly stdin.on("data", (key) => handleUserInput(key))
-  return stdin; // return the stdin object so we can use it elsewhere in the program
+
+  return stdin;
 };
 
-const handleUserInput = function(key) {
+const handleUserInput = function (key) {
   // code handling the key presses
+
   if (key === "\u0003") {
+    // CTRL + C
     console.log("Exiting Game...");
     process.exit();
   }
